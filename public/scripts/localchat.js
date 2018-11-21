b95aa2a7c50192c7c261d7c8e208eec6d9d6aa4d
Shashvat Srivastava <shashvat.sri@gmail.com>
2018-11-21 18:15:12 -0500

    added a semicolon

--- public/scripts/localchat.js
+++ public/scripts/localchat.js
@@ -12,6 +12,7 @@ socket.on('localmessage', string_data => {
     const data = JSON.parse(string_data);
     const container = document.getElementById('message-container');
     
-    
-    container.innerHTML += `<div><b> ${data.user}:</b> ${data.message} </div>`
+    //vulnerable to javascript injection
+    //but doesnt really matter; nothing to steal
+    container.innerHTML += `<div><b> ${data.user}:</b> ${data.message} </div>`;
 });
