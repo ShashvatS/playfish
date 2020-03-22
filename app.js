6f8ef76480fcdb31f5015f9b54c649d586ce87a7
Shashvat Srivastava <shashvat.sri@gmail.com>
2020-03-21 22:58:20 -0400

    fixed error with... error handler (still needs testing

--- app.js
+++ app.js
@@ -52,15 +52,17 @@ if (app.get('env') === 'development') {
         });
     });
 }
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+else {
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-});
+}
 app.set('port', process.env.PORT);
 server.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address());
